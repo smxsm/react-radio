e01cf78fa1070e7035a9ed00b7aeb92623a620ae
Ivan Ivanov <ivan.m.ivanov@icloud.com>
2023-03-20 20:55:17 +0200

    Added active prop

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -3,15 +3,19 @@ import styles from './Button.module.css';
 type ButtonProps = React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> & {
   loading?: boolean;
   error?: boolean;
+  active?: boolean;
 };
 
-export default function Button({ loading, error, className, ...props }: ButtonProps) {
+export default function Button({ loading, error, active = false, className, ...props }: ButtonProps) {
   let classes = `${styles.btn} ${className || ''}`.trim();
   if (loading) {
     classes += ' ' + styles.loading;
   }
   if (error) {
     classes += ' ' + styles.error;
+  }
+  if (active) {
+    classes += ' ' + styles.active;
   }
   return <button {...props} className={classes} />;
 }
