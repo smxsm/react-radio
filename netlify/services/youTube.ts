ef0f28cf152812141f1007488bc052e6b62e4a19
Ivan Ivanov <ivan.m.ivanov@icloud.com>
2023-03-25 23:16:29 +0200

    use regex to parse youtube page instead of api

--- netlify/services/youTube.ts
+++ netlify/services/youTube.ts
@@ -1,16 +1,13 @@
-const API_URL = 'https://www.googleapis.com/youtube/v3/search';
-const API_KEY = Deno.env.get('YOUTUBE_API_KEY') || '';
-
 export default async function youTubeSearch(searchTerm: string) {
   if (!searchTerm) return null;
   try {
-    console.log('calling youtube');
-    const res = await fetch(`${API_URL}?key=${API_KEY}&type=video&q=${searchTerm}`);
-    const data = await res.json();
-    if (!data.items?.length) {
+    const res = await fetch(`https://www.youtube.com/results?search_query=${searchTerm}`);
+    const html = await res.text();
+    const match = html.match(/(?:"videoId":")(?<videoId>.*?)(?:")/)?.groups;
+    if (!match?.videoId) {
       return null;
     }
-    return `https://www.youtube.com/watch?v=${data.items[0].id.videoId}`;
+    return `https://www.youtube.com/watch?v=${match.videoId}`;
   } catch (err) {
     return null;
   }
