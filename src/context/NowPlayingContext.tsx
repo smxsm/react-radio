6611c8feaa8ef0ea37d784d314fcc0e2d6713e1c
Ivan Ivanov <ivan.m.ivanov@icloud.com>
2023-03-29 20:29:03 +0300

    Abort pending fetch on play status or station change

--- src/context/NowPlayingContext.tsx
+++ src/context/NowPlayingContext.tsx
@@ -44,34 +44,31 @@ type TrackInfo = {
 
 type NowPlayingInfoProviderProps = PropsWithChildren & {};
 
-/**
- * Gets metadata for a radio stream from a free internet service
- * @param url The original stream URL
- * @returns A Promise whose resolved value is a StationMetadata object. Throws on error.
- */
-const getStationMetadata = async (url: string): Promise<StationMetadata> => {
-  const res = await fetch('https://radio.ivanoff.dev/station-metadata?url=' + url);
-  const data = await res.json();
-  return data;
-};
-
 export const NowPlayingContext = createContext<NowPlayingContextType | null>(null);
 
 export function NowPlayingProvider({ children }: NowPlayingInfoProviderProps) {
   const playerContext = useContext(PlayerContext);
   const [nowPlaying, setNowPlaying] = useState<NowPlayingContextType>({});
   const intervalRef = useRef<NodeJS.Timer | number>(0);
+  const abortControllerRef = useRef(new AbortController());
 
   useEffect(() => {
     const getNowPlayingInfo = async (url: string | undefined) => {
       if (!url) return;
       try {
-        const stationMetadata = await getStationMetadata(url);
+        abortControllerRef.current = new AbortController();
+        const res = await fetch('https://radio.ivanoff.dev/station-metadata?url=' + url, {
+          signal: abortControllerRef.current.signal,
+        });
+        const stationMetadata = await res.json();
         setNowPlaying((state) => ({ ...state, stationMetadata }));
       } catch (err) {}
     };
 
     clearInterval(intervalRef.current);
+    if (!abortControllerRef.current.signal.aborted) {
+      abortControllerRef.current.abort();
+    }
 
     if (playerContext?.status === 'stopped' || playerContext?.status === 'error') {
       setNowPlaying({});
