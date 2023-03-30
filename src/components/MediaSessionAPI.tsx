97b912a2bfee0428dd3aaae80f44423cc0027965
Ivan Ivanov <ivan.m.ivanov@icloud.com>
2023-03-30 21:36:15 +0300

    Rewritten

--- src/components/MediaSessionAPI.tsx
+++ src/components/MediaSessionAPI.tsx
@@ -3,37 +3,58 @@ import { NowPlayingContext } from '../context/NowPlayingContext';
 import { PlayerContext } from '../context/PlayerContext';
 
 export function MediaSessionAPI() {
-  const { status, play, stop, previous, next } = useContext(PlayerContext)!;
-  const nowPlaying = useContext(NowPlayingContext);
+  const { play, stop, previous, next } = useContext(PlayerContext) || {};
+  const { station, stationMetadata } = useContext(NowPlayingContext) || {};
+
+  const { artist, title, artwork } = stationMetadata?.trackMatch || {};
+  const { title: stationTitle, icyName } = stationMetadata || {};
+  const { name, logo } = station || {};
 
   useEffect(() => {
-    if (!('mediaSession' in navigator) || status !== 'playing') {
-      return;
-    }
-
-    navigator.mediaSession.playbackState = 'playing';
-
+    if (!('mediaSession' in navigator)) return;
+    if (!play) return;
     navigator.mediaSession.setActionHandler('play', () => play());
-    navigator.mediaSession.setActionHandler('stop', () => stop());
+
+    return () => navigator.mediaSession.setActionHandler('play', null);
+  }, [play]);
+
+  useEffect(() => {
+    if (!('mediaSession' in navigator)) return;
+    if (!stop) return;
+    navigator.mediaSession.setActionHandler('pause', () => stop());
+
+    return () => navigator.mediaSession.setActionHandler('pause', null);
+  }, [stop]);
+
+  useEffect(() => {
+    if (!('mediaSession' in navigator)) return;
+    if (!previous) return;
     navigator.mediaSession.setActionHandler('previoustrack', () => previous());
+
+    return () => navigator.mediaSession.setActionHandler('previoustrack', null);
+  }, [previous]);
+
+  useEffect(() => {
+    console.log('setting next');
+    if (!('mediaSession' in navigator)) return;
+    if (!next) return;
     navigator.mediaSession.setActionHandler('nexttrack', () => next());
 
+    return () => navigator.mediaSession.setActionHandler('nexttrack', null);
+  }, [next]);
+
+  useEffect(() => {
+    if (!('mediaSession' in navigator)) return;
+
     navigator.mediaSession.metadata = new MediaMetadata({
-      title: nowPlaying?.stationMetadata?.trackMatch?.title || nowPlaying?.stationMetadata?.title,
-      artist: nowPlaying?.stationMetadata?.trackMatch?.artist,
-      album: nowPlaying?.stationMetadata?.icyName || nowPlaying?.station?.name,
-      artwork: [
-        {
-          src:
-            nowPlaying?.stationMetadata?.trackMatch?.artwork.replace('100x100', '600x600') ||
-            nowPlaying?.station?.logo ||
-            '',
-        },
-      ],
+      title: title || stationTitle || '',
+      artist: artist || '',
+      album: icyName || name,
+      artwork: [{ src: artwork?.replace('100x100', '600x600') || logo || '' }],
     });
 
-    navigator.mediaSession.setPositionState({ duration: 0, playbackRate: 1 });
-  }, [nowPlaying, status, play, stop, next, previous]);
+    navigator.mediaSession.playbackState = 'playing';
+  }, [artist, title, artwork, stationTitle, icyName, name, logo]);
 
   return null;
 }
