6da0764f7d8ec00f1f673e4f3cdf92fde87f8264
Ivan Ivanov <ivan.m.ivanov@icloud.com>
2023-04-01 10:33:51 +0300

    Played with player elements positioning

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -2,7 +2,7 @@ import styles from './Player.module.css';
 
 import { useContext } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStepBackward, faPlay, faStepForward } from '@fortawesome/free-solid-svg-icons';
+import { faStepBackward, faPlay, faStepForward, faStop } from '@fortawesome/free-solid-svg-icons';
 import SpectrumAnalyzer from './SpectrumAnalyzer';
 import ScrollingText from './ui/ScrollingText';
 import { PlayerContext } from '../context/PlayerContext';
@@ -20,87 +20,86 @@ export default function Player(props: any) {
 
   return (
     <div className={classes}>
-      <div className={styles['now-playing-container']}>
+      {/* <div className={styles['now-playing-container']}>
         <div className={styles['now-playing-info']}>
           <ScrollingText text={stationMetadata?.icyName || station?.name || ''} className={styles['radio-name']} />
           <ScrollingText text={stationMetadata?.title || ''} className={styles['track-name']} />
         </div>
+      </div> */}
+
+      <div className={styles.currentTrack}>
+        <img
+          src={matchedTrack?.artwork.replace('100x100', '600x600') || station?.logo || '/radio-no-logo.png'}
+          alt=""
+          className={styles.artwork}
+        />
 
         <SpectrumAnalyzer
           source={playerContext?.sourceNode}
           audioCtx={playerContext?.audioContext}
           className={styles.visualizer}
         />
-      </div>
 
-      <div className={styles['media-controls']}>
-        <div className={styles['button-container']}>
-          <button className={styles['media-button']} onClick={() => playerContext.previous()}>
-            <FontAwesomeIcon icon={faStepBackward} className={styles['media-button-icon']} />
-          </button>
-          <button
-            className={`${styles['media-button']} ${styles['media-button-play']}`}
-            onClick={() => (playerContext.status !== 'stopped' ? playerContext.stop() : playerContext.play())}
-          >
-            <FontAwesomeIcon icon={faPlay} className={styles['media-button-icon']} />
-          </button>
-          <button className={styles['media-button']} onClick={() => playerContext.next()}>
-            <FontAwesomeIcon icon={faStepForward} className={styles['media-button-icon']} />
-          </button>
+        <div className={styles.trackInfo}>
+          <ScrollingText text={stationMetadata?.icyName || station?.name || ''} className={styles.stationName} />
+          <ScrollingText text={matchedTrack?.title || stationMetadata?.title || '-'} className={styles.trackTitle} />
+          <ScrollingText text={matchedTrack?.artist || ''} className={styles.trackArtist} />
         </div>
-      </div>
 
-      <img
-        src={matchedTrack?.artwork.replace('100x100', '600x600') || station?.logo || '/radio-no-logo.png'}
-        alt=""
-        className={styles.artwork}
-      />
+        <div className={styles['media-controls']}>
+          <div className={styles['button-container']}>
+            <button className={styles['media-button']} onClick={() => playerContext.previous()}>
+              <FontAwesomeIcon icon={faStepBackward} className={styles['media-button-icon']} />
+            </button>
+            <button
+              className={`${styles['media-button']} ${styles['media-button-play']}`}
+              onClick={() => (playerContext.status !== 'stopped' ? playerContext.stop() : playerContext.play())}
+            >
+              {playerContext.status === 'stopped' && (
+                <FontAwesomeIcon icon={faPlay} className={styles['media-button-icon']} />
+              )}
+              {playerContext.status !== 'stopped' && (
+                <FontAwesomeIcon icon={faStop} className={styles['media-button-icon']} />
+              )}
+            </button>
+            <button className={styles['media-button']} onClick={() => playerContext.next()}>
+              <FontAwesomeIcon icon={faStepForward} className={styles['media-button-icon']} />
+            </button>
+          </div>
+        </div>
 
-      <div className={styles.trackInfo}>
-        {matchedTrack && (
-          <>
-            <ScrollingText text={matchedTrack.title} className={styles.trackTitle} />
-            <ScrollingText text={matchedTrack.artist} className={styles.trackArtist} />
-            <ScrollingText
-              text={`${matchedTrack.album} (${matchedTrack.releaseDate?.getFullYear()})`}
-              className={styles.trackAlbum}
-            />
-          </>
-        )}
-      </div>
-
-      {matchedTrack && (
         <div className={styles.musicLinks}>
-          {matchedTrack.appleMusicUrl && (
+          {matchedTrack?.appleMusicUrl && (
             <a target="_blank" rel="noreferrer" href={matchedTrack.appleMusicUrl} onClick={() => playerContext?.stop()}>
               <img src="/apple-music.svg" alt="Apple Musc" />
             </a>
           )}
-          {matchedTrack.youTubeUrl && (
+          {matchedTrack?.youTubeUrl && (
             <a target="_blank" rel="noreferrer" href={matchedTrack.youTubeUrl} onClick={() => playerContext?.stop()}>
               <img src="/youtube.png" alt="Apple Musc" />
             </a>
           )}
         </div>
-      )}
+      </div>
 
-      {history && (
+      {/* {history && (
         <div className={styles.history}>
           {history.map((entry) =>
-            entry.status === 'playing' ? null : (
+            entry.id === matchedTrack?.id ? null : (
               <Card key={entry.id} className={styles.historyCard}>
                 <figure>
                   <img src={entry.artwork}></img>
                 </figure>
                 <div className={styles.historyTrackInfo}>
                   <p className={styles.historyCardTitle}>{entry.title}</p>
-                  <p className={styles.historyCardTitle}>{entry.artist}</p>
+                  <p className={styles.historyCardArtist}>{entry.artist}</p>
+                  <p className={styles.historyCardDate}>{entry.heardAt.toLocaleString()}</p>
                 </div>
               </Card>
             )
           )}
         </div>
-      )}
+      )} */}
     </div>
   );
 }
