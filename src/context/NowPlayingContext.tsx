55a560e6c0f696b37d56d8d709686e0a82894c82
Ivan Ivanov <ivan.m.ivanov@icloud.com>
2023-04-01 19:23:55 +0300

    Set song history state when user present

--- src/context/NowPlayingContext.tsx
+++ src/context/NowPlayingContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, PropsWithChildren, useContext, useEffect, useRef, useState } from 'react';
 import useSupabase from '../hooks/useSupabase';
 import { PlayerContext } from './PlayerContext';
+import { UserContext } from './UserContext';
 
 type NowPlayingContextType = {
   station?: RadioStation;
@@ -46,7 +47,6 @@ type TrackInfo = {
 };
 
 type TrackHistory = TrackInfo & {
-  status: 'playing' | 'history';
   heardAt: Date;
 };
 
@@ -57,6 +57,7 @@ export const NowPlayingContext = createContext<NowPlayingContextType | null>(nul
 export function NowPlayingProvider({ children }: NowPlayingInfoProviderProps) {
   const supabase = useSupabase();
   const playerContext = useContext(PlayerContext);
+  const { user } = useContext(UserContext) || {};
   const [station, setStation] = useState<RadioStation | undefined>();
   const [stationMetadata, setStationMetadata] = useState<StationMetadata | undefined>();
   const [matchedTrack, setMatchedTrack] = useState<TrackInfo | undefined>();
@@ -129,7 +130,6 @@ export function NowPlayingProvider({ children }: NowPlayingInfoProviderProps) {
         if (error) throw error;
         return data.map<TrackHistory>((entry) => ({
           heardAt: new Date(entry.created_at),
-          status: entry.track_id === matchedTrack?.id ? 'playing' : 'history',
           id: entry.track_match.id,
           artist: entry.track_match.artist,
           title: entry.track_match.title,
@@ -140,6 +140,28 @@ export function NowPlayingProvider({ children }: NowPlayingInfoProviderProps) {
       })
       .then(setHistory);
   }, [supabase, matchedTrack?.id]);
+
+  useEffect(() => {
+    if (!user) return setHistory([]);
+    supabase
+      .from('tracks_history')
+      .select('*, track_match (id, artist, title, album, artwork, release_date)')
+      .order('created_at', { ascending: false })
+      .limit(100)
+      .then(({ data, error }) => {
+        if (error) throw error;
+        return data.map<TrackHistory>((entry) => ({
+          heardAt: new Date(entry.created_at),
+          id: entry.track_match.id,
+          artist: entry.track_match.artist,
+          title: entry.track_match.title,
+          album: entry.track_match.album,
+          releaseDate: new Date(entry.track_match.release_date),
+          artwork: entry.track_match.artwork,
+        }));
+      })
+      .then(setHistory);
+  }, [supabase, user]);
 
   return (
     <NowPlayingContext.Provider value={{ station, stationMetadata, matchedTrack, history }}>
