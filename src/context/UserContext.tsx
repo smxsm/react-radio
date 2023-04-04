47570008672d0057f2a3b9609d8990749ce376d2
Ivan Ivanov <ivan.m.ivanov@icloud.com>
2023-04-04 21:26:34 +0300

    Rework auth logic

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -26,86 +26,80 @@ export function UserProvider({ children }: UserProviderProps) {
   const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
+  const [persistSession, setPersistSession] = useState(true);
 
   useEffect(() => {
+    supabase.auth.onAuthStateChange(async (_, session) => {
+      if (!session) {
+        setUser(null);
+        setLoading(false);
+        localStorage.removeItem('session');
+        return;
+      }
+      setLoading(true);
+      const { data, error } = await supabase.from('profiles').select('*');
+      if (error || !data.length) {
+        setLoading(false);
+        return;
+      }
+      if (persistSession) {
+        localStorage.setItem('session', JSON.stringify(session));
+      }
+      const [{ id, email, first_name: firstName, last_name: lastName }] = data;
+      setUser({ id, email, firstName, lastName });
+      setLoading(false);
+      setError(null);
+    });
+
     const session = JSON.parse(localStorage.getItem('session') || 'null');
     if (!session) return setLoading(false);
     setLoading(true);
-    supabase.auth
-      .setSession(session)
-      .then(() => supabase.auth.refreshSession())
-      .then(() => supabase.from('profiles').select('*'))
-      .then(({ data }) => {
-        if (!data?.length) return;
-        setUser({
-          id: data[0].id,
-          email: data[0].email,
-          firstName: data[0]['first_name'],
-          lastName: data[0]['last_name'],
-        });
-      })
-      .finally(() => setLoading(false));
-  }, [supabase]);
+    supabase.auth.setSession(session).then(({ error }) => {
+      if (error) {
+        localStorage.removeItem('session');
+        setLoading(false);
+      }
+    });
+  }, [supabase, persistSession]);
 
-  const signup = (email: string, firstName: string, lastName: string, password: string) => {
-    setError(null);
+  const signup = async (email: string, firstName: string, lastName: string, password: string) => {
     setLoading(true);
-    supabase.auth
-      .signUp({ email, password })
-      .then(({ data, error }) => {
-        if (error) throw error;
-        return data;
-      })
-      .then(({ user }) =>
-        supabase.from('profiles').insert({
-          id: user!.id,
-          email,
-          first_name: firstName,
-          last_name: lastName,
-        })
-      )
-      .then(({ error }) => {
-        if (error) throw error;
-      })
-      .then(() => signin(email, password, true))
-      .catch(setError)
-      .finally(() => setLoading(false));
+    const { data, error: signUpError } = await supabase.auth.signUp({ email, password });
+    if (signUpError) {
+      setError(new Error(signUpError.message));
+      setLoading(false);
+      return;
+    }
+    const { error: createProfileError } = await supabase.from('profiles').insert({
+      id: data.user!.id,
+      email,
+      first_name: firstName,
+      last_name: lastName,
+    });
+    if (createProfileError) {
+      setError(new Error(createProfileError.message));
+      setLoading(false);
+      return;
+    }
+    signin(email, password, true);
   };
 
-  const signin = (email: string, password: string, saveSession: boolean) => {
+  const signin = async (email: string, password: string, persistSession: boolean = true) => {
     setError(null);
     setLoading(true);
-    supabase.auth
-      .signInWithPassword({ email, password })
-      .then(({ data, error }) => {
-        if (error) throw error;
-
-        if (saveSession) {
-          localStorage.setItem('session', JSON.stringify(data.session));
-        }
-        return supabase.from('profiles').select('*');
-      })
-      .then(({ data, error }) => {
-        if (error || !data || !data.length) throw error || new Error('Error loading profile information');
-        const { id, email, first_name: firstName, last_name: lastName } = data[0];
-        setUser({ id, email, firstName, lastName });
-      })
-      .catch(setError)
-      .finally(() => setLoading(false));
+    setPersistSession(persistSession);
+    const { error } = await supabase.auth.signInWithPassword({ email, password });
+    if (error) {
+      setLoading(false);
+      setError(new Error(error.message));
+      return;
+    }
   };
 
   const signout = () => {
     setError(null);
     setLoading(true);
-    supabase.auth
-      .signOut()
-      .then(({ error }) => {
-        localStorage.removeItem('session');
-        setUser(null);
-        if (error) throw error;
-      })
-      .catch(setError)
-      .finally(() => setLoading(false));
+    supabase.auth.signOut();
   };
 
   return (
