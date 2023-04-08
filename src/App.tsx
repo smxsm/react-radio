a4ff960a55062a0271c83bc11944598e06208cbc
Ivan Ivanov <ivan.m.ivanov@icloud.com>
2023-04-08 21:13:22 +0300

    Added context provider to set document title

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { UserProvider } from './context/UserContext';
+import { PlayerProvider } from './context/PlayerContext';
+import { NowPlayingProvider } from './context/NowPlayingContext';
+import { DocumentTitleProvider } from './context/DocumentTitleContext';
+
 import StationsCardList from './components/StationsCardList';
-import styles from './App.module.css';
 import Player from './components/Player';
 import StationsMenu from './components/StationsMenu';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CountriesList from './components/CountriesList';
 import GenresList from './components/GenresList';
 import Home from './components/Home';
-import { PlayerProvider } from './context/PlayerContext';
-import { NowPlayingProvider } from './context/NowPlayingContext';
 import { MediaSessionAPI } from './components/MediaSessionAPI';
 import UserMenu from './components/UserMenu';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
-import { UserProvider } from './context/UserContext';
 import SearchForm from './components/SearchForm';
 import UpsertCustomStation from './components/UpsertCustomStation';
 import CustomStations from './components/CustomStations';
 import TrackHistory from './components/TrackHistory';
 import NotFound from './components/NotFound';
 import ProtectedRoute from './components/ProtectedRoute';
+
+import styles from './App.module.css';
 
 function App() {
   return (
@@ -35,24 +39,26 @@ function App() {
               </nav>
               <div className={styles.container}>
                 <main className={styles.content}>
-                  <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/stations/all" element={<StationsCardList />} />
-                    <Route path="/stations/countries" element={<CountriesList />} />
-                    <Route path="/stations/music/genres" element={<GenresList />} />
-                    <Route path="/stations/music/:category?/:value?" element={<StationsCardList />} />
-                    <Route path="/stations/:category?/:value?" element={<StationsCardList />} />
-                    <Route element={<ProtectedRoute hasUser={true} redirectTo={'/auth/signin'} />}>
-                      <Route path="/stations/custom" element={<CustomStations />} />
-                      <Route path="/stations/custom/add" element={<UpsertCustomStation />} />
-                      <Route path="/stations/custom/edit/:id" element={<UpsertCustomStation />} />
-                    </Route>
-                    <Route element={<ProtectedRoute hasUser={false} redirectTo={'/'} />}>
-                      <Route path="/auth/signin" element={<SignIn />} />
-                      <Route path="/auth/signup" element={<SignUp />} />
-                    </Route>
-                    <Route path="*" element={<NotFound />} />
-                  </Routes>
+                  <DocumentTitleProvider>
+                    <Routes>
+                      <Route path="/" element={<Home />} />
+                      <Route path="/stations/all" element={<StationsCardList />} />
+                      <Route path="/stations/countries" element={<CountriesList />} />
+                      <Route path="/stations/music/genres" element={<GenresList />} />
+                      <Route path="/stations/music/:category?/:value?" element={<StationsCardList />} />
+                      <Route path="/stations/:category?/:value?" element={<StationsCardList />} />
+                      <Route element={<ProtectedRoute hasUser={true} redirectTo={'/auth/signin'} />}>
+                        <Route path="/stations/custom" element={<CustomStations />} />
+                        <Route path="/stations/custom/add" element={<UpsertCustomStation />} />
+                        <Route path="/stations/custom/edit/:id" element={<UpsertCustomStation />} />
+                      </Route>
+                      <Route element={<ProtectedRoute hasUser={false} redirectTo={'/'} />}>
+                        <Route path="/auth/signin" element={<SignIn />} />
+                        <Route path="/auth/signup" element={<SignUp />} />
+                      </Route>
+                      <Route path="*" element={<NotFound />} />
+                    </Routes>
+                  </DocumentTitleProvider>
                 </main>
                 <aside className={styles.sidePanel}>
                   <Player />
---
+++ src/context/DocumentTitleContext.tsx
@@ -0,0 +1,51 @@
+import { PropsWithChildren, createContext, useCallback, useContext, useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
+import { PlayerContext } from './PlayerContext';
+import { NowPlayingContext } from './NowPlayingContext';
+
+type DocumentTitleContextType = {
+  setDocumentTitle: (title: string) => void;
+};
+
+type DocumentTitleProviderProps = PropsWithChildren & {};
+
+type LocationTitle = {
+  [key: string]: string;
+};
+
+export const DocumentTitleContext = createContext<DocumentTitleContextType | null>(null);
+
+export function DocumentTitleProvider({ children }: DocumentTitleProviderProps) {
+  const [locationTitle, setLocationTitle] = useState<LocationTitle>({});
+  const { status } = useContext(PlayerContext) || {};
+  const { station, stationMetadata, matchedTrack } = useContext(NowPlayingContext) || {};
+  const locationKey = useLocation().key;
+
+  useEffect(() => {
+    const title = locationTitle[locationKey];
+    let newTitle = (title && `${title} | React Radio`) || 'React Radio';
+    if (status === 'playing') {
+      const stationName = stationMetadata?.icyName || station?.name;
+      if (matchedTrack?.artist && matchedTrack.title) {
+        newTitle += ` - Playing ${matchedTrack.title} by ${matchedTrack.artist} on ${stationName}`;
+      } else {
+        newTitle += ` - Playing ${stationName}`;
+      }
+    }
+
+    document.title = newTitle;
+  }, [
+    locationTitle,
+    locationKey,
+    status,
+    station?.name,
+    stationMetadata?.icyName,
+    matchedTrack?.artist,
+    matchedTrack?.title,
+  ]);
+
+  const setDocumentTitle = useCallback((title: string) => setLocationTitle({ [locationKey]: title }), [locationKey]);
+
+  return <DocumentTitleContext.Provider value={{ setDocumentTitle }}>{children}</DocumentTitleContext.Provider>;
+}
